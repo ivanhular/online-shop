f1871ebb4ed888af915c1c96d3bf68d2ba677424
ivan <ivan.hular@roya.com>
2019-11-21 15:13:31 +0800

    Added model

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const User = require('./models/user')
 
 const port = process.env.PORT || 5000
 
@@ -6,8 +7,15 @@ const app = express()
 app.use(express.json())
 
 
-app.get('/', (req, res) => {
-    res.send({name:'test'})
+
+app.post('/users', async (req, res) => {
+
+    // console.log(user)
+    const user = await new User(req.body)
+
+    await user.save()
+    
+    res.send(user)
 })
 
 app.listen(port, () => {
